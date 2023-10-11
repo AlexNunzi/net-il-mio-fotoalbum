acfe770374ee3c23e74fbe2f13fb278b842528e3
AlexNunzi <alex.nunzi@hotmail.it>
2023-10-11 16:35:03 +0200

    Aggiunta pagina di creazione di una nuova foto

--- net-il-mio-fotoalbum/Controllers/PhotoController.cs
+++ net-il-mio-fotoalbum/Controllers/PhotoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using net_il_mio_fotoalbum.Database;
 using net_il_mio_fotoalbum.Models;
@@ -23,8 +24,6 @@ namespace net_il_mio_fotoalbum.Controllers
         [HttpGet]
         public IActionResult Details(long id)
         {
-
-
             Photo? detailedPhoto = _photoDatabase.Photos.Where(photo => photo.Id == id).Include(photo => photo.Categories).FirstOrDefault();
 
             if (detailedPhoto == null)
@@ -35,8 +34,93 @@ namespace net_il_mio_fotoalbum.Controllers
             {
                 return View("Details", detailedPhoto);
             }
+        }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            PhotoFormModel formModel = new PhotoFormModel();
 
+            List<Category> dbCategories = _photoDatabase.Categories.ToList();
+
+            List<SelectListItem> availableCategories = new List<SelectListItem>();
+
+            if (dbCategories != null)
+            {
+                foreach (Category category in dbCategories)
+                {
+                    string ingredientStringId = category.Id.ToString();
+
+                    availableCategories.Add(new SelectListItem { Value = ingredientStringId, Text = category.Name });
+                }
+            }
+
+            formModel.newPhoto = new Photo();
+            formModel.AvailableCategories = availableCategories;
+
+            return View("Create", formModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(PhotoFormModel formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<Category> dbCategories = _photoDatabase.Categories.ToList();
+
+                List<SelectListItem> availableCategories = new List<SelectListItem>();
+
+                if (dbCategories != null)
+                {
+                    foreach (Category ingredient in dbCategories)
+                    {
+                        string ingredientStringId = ingredient.Id.ToString();
+
+                        availableCategories.Add(new SelectListItem { Value = ingredientStringId, Text = ingredient.Name });
+                    }
+                }
+
+                formData.AvailableCategories = availableCategories;
+
+                return View("Create", formData);
+            }
+
+            formData.newPhoto.Categories = new List<Category>();
+
+            if (formData.SelectedCategories != null && formData.SelectedCategories.Count > 0)
+            {
+                foreach(string categoryId in formData.SelectedCategories)
+                {
+                    long parsedCategoryId = long.Parse(categoryId);
+
+                    Category? dbCategory = _photoDatabase.Categories.Where(c => c.Id == parsedCategoryId).FirstOrDefault();
+
+                    if (dbCategory != null)
+                    {
+                        formData.newPhoto.Categories.Add(dbCategory);
+                    }
+                }
+            }
+
+            SetImageFileFromFormFile(formData);
+
+            _photoDatabase.Add(formData.newPhoto);
+            _photoDatabase.SaveChanges();
+
+            return RedirectToAction("Index", "Photo");
+        }
+
+        private void SetImageFileFromFormFile(PhotoFormModel formData)
+        {
+            if (formData.ImageFormFile == null)
+            {
+                return;
+            }
+
+            MemoryStream stream = new MemoryStream();
+            formData.ImageFormFile.CopyTo(stream);
+            formData.newPhoto.ImageFile = stream.ToArray();
         }
     }
 }
---
+++ net-il-mio-fotoalbum/Models/PhotoFormModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class PhotoFormModel
+    {
+        public Photo newPhoto { get; set; }
+        public IFormFile? ImageFormFile { get; set; }
+        public List<SelectListItem>? AvailableCategories { get; set; }
+        public List<string>? SelectedCategories { get; set; }
+
+
+    }
+}
