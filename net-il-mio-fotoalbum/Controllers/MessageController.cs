19a12894cadb8ab7df1eadf9087639b23e037bed
AlexNunzi <alex.nunzi@hotmail.it>
2023-10-13 18:01:50 +0200

    Aggiunta pagina di visualizzazione dei messaggi ricevuti dalla pagina di contatto dall'utente

---
+++ net-il-mio-fotoalbum/Controllers/MessageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Database;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Controllers
+{
+    public class MessageController : Controller
+    {
+        private PhotoContext _photoDatabase;
+
+        public MessageController(PhotoContext photoDatabase)
+        {
+            _photoDatabase = photoDatabase;
+        }
+
+        [Authorize(Roles = "ADMIN, USER")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Message> messages = _photoDatabase.Messages.ToList();
+            return View("Index", messages);
+        }
+    }
+}
